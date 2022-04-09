a9273664deeb644a090f7e5bb84d90d968c4545f
王麒雄（实习） <wangqixiong@xiaohongshu.com>
2022-04-09 23:53:06 +0800

    debug

--- configs/oriented_rcnn/oriented_rcnn_r50_fpn_3x_isprs_airplane_le90.py
+++ configs/oriented_rcnn/oriented_rcnn_r50_fpn_3x_isprs_airplane_le90.py
@@ -1,5 +1,6 @@
 _base_ = [
-    '../_base_/datasets/dotav1.py', '../_base_/schedules/schedule_3x.py',
+    # '../_base_/datasets/dotav1.py', 
+    '../_base_/schedules/schedule_3x.py',
     '../_base_/default_runtime.py'
 ]
 
