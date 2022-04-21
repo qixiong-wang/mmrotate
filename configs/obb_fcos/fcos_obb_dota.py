7895df35cb280755c40823e46ab4060329628fab
王麒雄（实习） <wangqixiong@xiaohongshu.com>
2022-04-22 01:57:15 +0800

    debnug

--- configs/obb_fcos/fcos_obb_dota.py
+++ configs/obb_fcos/fcos_obb_dota.py
@@ -62,45 +62,33 @@ img_norm_cfg = dict(
     mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadOBBAnnotations', with_bbox=True,
-         with_label=True, obb_as_mask=True),
-    dict(type='LoadDOTASpecialInfo'),
-    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
-    dict(type='OBBRandomFlip', h_flip_ratio=0.5, v_flip_ratio=0.5),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(1024, 1024)),
+    dict(
+        type='RRandomFlip',
+        flip_ratio=[0.25, 0.25, 0.25],
+        direction=['horizontal', 'vertical', 'diagonal'],
+        version=angle_version),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='RandomOBBRotate', rotate_after_flip=True,
-         angles=(0, 0), vert_rate=0.5, vert_cls=['roundabout', 'storage-tank']),
     dict(type='Pad', size_divisor=32),
-    dict(type='DOTASpecialIgnore', ignore_size=2),
-    dict(type='FliterEmpty'),
-    dict(type='Mask2OBB', obb_type='obb'),
-    dict(type='OBBDefaultFormatBundle'),
-    dict(type='OBBCollect', keys=['img', 'gt_bboxes', 'gt_obboxes', 'gt_labels'])
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
-        type='MultiScaleFlipRotateAug',
-        img_scale=[(1024, 1024)],
-        h_flip=False,
-        v_flip=False,
-        rotate=False,
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='OBBRandomFlip'),
+            dict(type='RResize'),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='RandomOBBRotate', rotate_after_flip=True),
             dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='OBBCollect', keys=['img']),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
         ])
 ]
-data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=4,
-    train=dict(pipeline=train_pipeline),
-    val=dict(pipeline=test_pipeline),
-    test=dict(pipeline=test_pipeline))
+
 # optimizer
 optimizer = dict(
     lr=0.0025, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
