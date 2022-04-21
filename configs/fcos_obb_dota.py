0db189ce3716afbdab4cffe220bbb949272b08b2
王麒雄（实习） <wangqixiong@xiaohongshu.com>
2022-04-22 00:19:35 +0800

    debug

--- configs/fcos_obb_dota.py
+++ configs/fcos_obb_dota.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/datasets/dota_v1.py',
     '../_base_/schedules/schedule_1x.py',
-    '../../_base_/default_runtime.py'
+    '../_base_/default_runtime.py'
 ]
 # model settings
 model = dict(
