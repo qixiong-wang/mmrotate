3f46267d01eee13785d298d9e43b142ac7a2bba9
王麒雄（实习） <wangqixiong@xiaohongshu.com>
2022-05-05 16:15:47 +0800

    debug

--- mmrotate/models/roi_heads/bbox_heads/rotated_bbox_head.py
+++ mmrotate/models/roi_heads/bbox_heads/rotated_bbox_head.py
@@ -119,7 +119,7 @@ class RotatedBBoxHead(BaseModule):
                         type='Normal', std=0.001, override=dict(name='fc_reg'))
                 ]
         # self.large_batch_queue=Large_batch_queue(num_classes=37,number_of_instance=64, feat_len=1024)
-        self.large_batch_queue=Large_batch_queue_classwise(num_classes=37,number_of_instance=16, feat_len=1024)
+        self.large_batch_queue=Large_batch_queue_classwise(num_classes=37,number_of_instance=64, feat_len=1024)
 
         # self.large_batch_queue=Large_batch_queue(num_classes=37,number_of_instance=16).cuda()
         # self.loss_batch_tri = TripletLossbatch()
@@ -340,9 +340,11 @@ class RotatedBBoxHead(BaseModule):
 
                 # large_batch_queue,queue_label = self.large_batch_queue(pos_feats, pos_labels)
                 # loss_batch_tri=self.loss_batch_tri(pos_feats, pos_labels,large_batch_queue,queue_label)
+                device = pos_labels.get_device()
+                
                 large_batch_queue = self.large_batch_queue(pos_feats, pos_labels)
                 loss_batch_tri=self.loss_batch_tri(pos_feats, pos_labels,large_batch_queue)
-                # device = loss_batch_tri.get_device()
+
                 losses['loss_triplet'] = loss_batch_tri
 
                 if isinstance(loss_cls_, dict):
